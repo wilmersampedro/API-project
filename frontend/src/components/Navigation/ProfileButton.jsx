2bbdf7601a0e67f623c4d6bf3f0fb605f0298f29
Wilmer <wilmer.sampedro@gmail.com>
2023-12-15 13:30:11 -0800

    dropdown menu

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -6,18 +6,21 @@ import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+
   return (
     <>
-      <button>
+      <button onClick={() => setShowMenu(!showMenu)}>
         <i className="fas fa-user-circle" />
       </button>
-      <ul className="profile-dropdown">
+      <ul className={ulClassName}>
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
