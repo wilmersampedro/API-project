d7c9a7c40f3a8abf49197136c7c8619d5ca46cbc
Wilmer <wilmer.sampedro@gmail.com>
2023-12-14 18:39:23 -0800

    session actions and reducer

---
+++ frontend/src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import './index.css';
+import configureStore from './store';
+
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
+
+const store = configureStore();
+
+// if (process.env.NODE_ENV !== 'production') {
+//   window.store = store;
+// }
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
