d347e3dddf7681ac4b6be984b3d7d60ed8412f0d
Wilmer <wilmer.sampedro@gmail.com>
2023-12-15 12:54:21 -0800

    logout thunk

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -59,6 +59,14 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE'
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 //initialize state
 const initialState = { user: null };
 
