03ce759e4b691f373300c676a22fbb55c963d58a
Wilmer <wilmer.sampedro@gmail.com>
2023-12-30 19:13:12 -0800

    still broken

--- frontend/src/components/CreateNewSpot/CreateNewSpot.jsx
+++ frontend/src/components/CreateNewSpot/CreateNewSpot.jsx
@@ -5,232 +5,243 @@ import { useNavigate } from "react-router-dom";
 import './CreateNewSpotForm.css'
 
 const CreateNewSpot = () => {
-const dispatch = useDispatch();
-const navigate = useNavigate();
-const [country, setCountry] = useState('');
-const [address, setAddress] = useState('');
-const [city, setCity] = useState('');
-const [state, setState] = useState('');
-const [description, setDescription] = useState('');
-const [name, setName] = useState('');
-const [price, setPrice] = useState('');
-const [errors, setErrors] = useState({})
-const [image1, setImage1] = useState('');
-const [image2, setImage2] = useState('');
-const [image3, setImage3] = useState('');
-const [image4, setImage4] = useState('');
-const [image5, setImage5] = useState('');
-const [submitted, setSubmitted] = useState(false)
-const imagesArr = [];
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [country, setCountry] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [description, setDescription] = useState('');
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [image1, setImage1] = useState('');
+  const [image2, setImage2] = useState('');
+  const [image3, setImage3] = useState('');
+  const [image4, setImage4] = useState('');
+  const [image5, setImage5] = useState('');
+  const [errors, setErrors] = useState({})
+  const [submitted, setSubmitted] = useState(false)
+  const imagesArr = [];
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  setSubmitted(true)
-  // setErrors({});
-  const spot = {
-    address,
-    city,
-    state,
-    country,
-    lat: 0,
-    lng: 0,
-    name,
-    description,
-    price
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSubmitted(true)
+    console.log("ERRORS IN HANDLE SUBMIT",errors)
+    // if (!image1) {
+    //   setErrors({ image1: "Preview Image required" })
+    //   console.log("ERRORS IN HANDLE SUBMIT", errors)
+    // }
 
-  const newSpot = await dispatch(thunkCreateSpot(spot))
-  console.log("IN THE NEWSPOT COMP", newSpot)
-  console.log("ERROR OBJ IN COMP", newSpot.errors)
-  if(newSpot.errors) {
-    setErrors({...newSpot.errors, ...errors})
-    console.log("ERROR IN COMPONENT", errors)
-  } else {
-    if (image1) imagesArr.push(image1)
-    if (image2) imagesArr.push(image2)
-    if (image3) imagesArr.push(image3)
-    if (image4) imagesArr.push(image4)
-    if (image5) imagesArr.push(image5)
-
-    console.log("IMAGES ARR", imagesArr)
-    console.log(newSpot)
-
-    const newImg = dispatch(thunkAddImageToSpot(newSpot.id, imagesArr))
-
-    const validNewSpot = {
-      ...newSpot,
-      newImg
+    // setErrors({});
+    const spot = {
+      address,
+      city,
+      state,
+      country,
+      lat: 0,
+      lng: 0,
+      name,
+      description,
+      price
     }
 
-    dispatch(thunkAddNewSpotToStore(validNewSpot));
+    const newSpot = await dispatch(thunkCreateSpot(spot))
+    console.log("IN THE NEWSPOT COMP", newSpot)
+    console.log("ERROR OBJ IN COMP", newSpot.errors)
+    if (newSpot.errors) {
+      console.log("🚀 ~ file: CreateNewSpot.jsx:52 ~ handleSubmit ~ newSpot:", newSpot.errors)
+      setErrors({ ...newSpot.errors, ...errors })
+      console.log("ERROR IN COMPONENT", errors)
+    } else {
+      if (image1) imagesArr.push(image1)
+      if (image2) imagesArr.push(image2)
+      if (image3) imagesArr.push(image3)
+      if (image4) imagesArr.push(image4)
+      if (image5) imagesArr.push(image5)
 
-    setCountry('')
-    setAddress('')
-    setCity('')
-    setState('')
-    setDescription('')
-    setName('')
-    setPrice('')
-    setImage1('')
-    setImage2('')
-    setImage3('')
-    setImage4('')
-    setImage5('')
+      console.log("IMAGES ARR", imagesArr)
+      console.log(newSpot)
 
-    navigate(`/spots/${validNewSpot.id}`)
+      const newImg = dispatch(thunkAddImageToSpot(newSpot.id, imagesArr))
+
+      const validNewSpot = {
+        ...newSpot,
+        newImg
+      }
+
+      dispatch(thunkAddNewSpotToStore(validNewSpot));
+
+      setCountry('')
+      setAddress('')
+      setCity('')
+      setState('')
+      setDescription('')
+      setName('')
+      setPrice('')
+      setImage1('')
+      setImage2('')
+      setImage3('')
+      setImage4('')
+      setImage5('')
+
+      navigate(`/spots/${validNewSpot.id}`)
+    }
   }
-}
 
-useEffect(() => {
-  const validationErrors = {}
+  useEffect(() => {
+    const validationErrors = {}
 
-  if(description.length < 30) validationErrors.description = "Description needs a minimum of 30 characters"
+    if (description.length < 30) {
+      validationErrors.description = "Description needs a minimum of 30 characters"
+    }
 
-  if(submitted && !image1) validationErrors.image1 = "Preview Image is required"
+    if (submitted && !image1) {
+      validationErrors.image1 = "Preview Image is required"
+    }
 
-  setErrors(validationErrors)
-},[image1, description.length, submitted])
+    setErrors(validationErrors)
+  }, [image1, description.length, submitted])
 
-return (
-  <div id="create-spot-container">
+  return (
+    <div id="create-spot-container">
 
-  <form onSubmit={handleSubmit} id="create-spot-form">
-    <h2 id="create-spot-header">Create a new Spot</h2>
-    <h3 id="create-spot-located-header">Where&apos;s your place located?</h3>
-    <h4 id="create-spot-guests-header">Guests will only get your exact address once they booked a reservation</h4>
-    <label id="country-label">
-      <div>
-      Country {"country" in errors && <span style={{color:"red"}}>{errors.country}</span>}
-      </div>
-      <input
-      id="country-input"
-      type="text"
-      placeholder="Country"
-      value={country}
-      onChange={(e) => setCountry(e.target.value)}
-      />
-    </label>
-    <label id="address-label">
-      <div>
-      Street Address {"address" in errors && <span style={{color:"red"}}>{errors.address}</span>}
-      </div>
-      <input
-      id="address-input"
-      type="text"
-      placeholder="Address"
-      value={address}
-      onChange={(e) => setAddress(e.target.value)}
-      />
-    </label>
-    <div id="city-state-div">
-    <label id="city-label">
-      <div>
-      City {"city" in errors && <span style={{color:"red"}}>{errors.city}</span>}
-      </div>
-      <input
-      id="city-input"
-      type="text"
-      placeholder="City"
-      value={city}
-      onChange={(e) => setCity(e.target.value)}
-      />
-    </label>
-    <span id="comma-city-state">,</span>
-    <label id="state-label">
-      <div>
-      State {"state" in errors && <span style={{color:"red"}}>{errors.state}</span>}
-      </div>
-      <input
-      id="state-input"
-      type="text"
-      placeholder="STATE"
-      value={state}
-      onChange={(e) => setState(e.target.value)}
-      />
-    </label>
+      <form onSubmit={handleSubmit} id="create-spot-form">
+        <h2 id="create-spot-header">Create a new Spot</h2>
+        <h3 id="create-spot-located-header">Where&apos;s your place located?</h3>
+        <h4 id="create-spot-guests-header">Guests will only get your exact address once they booked a reservation</h4>
+        <label id="country-label">
+          <div>
+            Country {"country" in errors && <span style={{ color: "red" }}>{errors.country}</span>}
+          </div>
+          <input
+            id="country-input"
+            type="text"
+            placeholder="Country"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
+        </label>
+        <label id="address-label">
+          <div>
+            Street Address {"address" in errors && <span style={{ color: "red" }}>{errors.address}</span>}
+          </div>
+          <input
+            id="address-input"
+            type="text"
+            placeholder="Street Address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </label>
+        <div id="city-state-div">
+          <label id="city-label">
+            <div>
+              City {"city" in errors && <span style={{ color: "red" }}>{errors.city}</span>}
+            </div>
+            <input
+              id="city-input"
+              type="text"
+              placeholder="City"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </label>
+          <span id="comma-city-state">,</span>
+          <label id="state-label">
+            <div>
+              State {"state" in errors && <span style={{ color: "red" }}>{errors.state}</span>}
+            </div>
+            <input
+              id="state-input"
+              type="text"
+              placeholder="STATE"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+            />
+          </label>
 
+        </div>
+        <label className="create-border-bottom">
+          <h2 id="describe-place-header">Describe your place to guests</h2>
+          <h4 className="create-form-subheader">Mention the best features of your space, any special amenities like fast wifi or parking, and what you love about the neighborhood</h4>
+          <textarea
+            id="create-text-area"
+            value={description}
+            placeholder="Please write at least 30 characters"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          {submitted && <span style={{ color: "red" }}>{errors.description}</span>}
+        </label>
+        <label className="create-border-bottom">
+          <h2>Create a title for your spot</h2>
+          <h4 className="create-form-subheader">Catch guests&apos; attention with a spot title that highlights what makes your place special</h4>
+          <input
+            id="spot-name-input"
+            type="text"
+            value={name}
+            placeholder="Name of your spot"
+            onChange={(e) => setName(e.target.value)}
+          />
+          {"name" in errors && <span style={{ color: "red" }}>{errors.name}</span>}
+        </label>
+        <label className="create-border-bottom">
+          <h2>Set a base price for your spot</h2>
+          <h4>Competitive pricing can help your listing stand out and rank higher in search results.</h4>
+          <span id="money">$ </span>
+          <input
+            id="price-input"
+            type="number"
+            value={price}
+            step={1}
+            min={1}
+            placeholder="Price per night (USD)"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          {"price" in errors && <span style={{ color: "red" }}>{errors.price}</span>}
+        </label>
+        <label className="create-border-bottom" id="photo-inputs">
+          <h2 id="photo-header-form">Liven up your spot with photos</h2>
+          <h4 className="create-form-subheader">Submit a link to at least one photo to publish your spot</h4>
+          <input
+            id="preview-image-url"
+            placeholder="Preview Image URL"
+            value={image1}
+            onChange={(e) => setImage1(e.target.value)}
+          />
+          {(submitted && "image1" in errors) && <span style={{ color: "red" }}>{errors.image1}</span>}
+          <input
+            className="image-url-input"
+            placeholder="Image URL"
+            type="text"
+            value={image2}
+            onChange={(e) => setImage2(e.target.value)}
+          />
+          <input
+            className="image-url-input"
+            placeholder="Image URL"
+            type="text"
+            value={image3}
+            onChange={(e) => setImage3(e.target.value)}
+          />
+          <input
+            className="image-url-input"
+            placeholder="Image URL"
+            type="text"
+            value={image4}
+            onChange={(e) => setImage4(e.target.value)}
+          />
+          <input
+            className="image-url-input"
+            placeholder="Image URL"
+            type="text"
+            value={image5}
+            onChange={(e) => setImage5(e.target.value)}
+          />
+        </label>
+        <button id="submit-create-spot" type="submit">Create Spot</button>
+      </form>
     </div>
-    <label className="create-border-bottom">
-      <h2 id="describe-place-header">Describe your place to guests</h2>
-      <h4 className="create-form-subheader">Mention the best features of your space, any special amenities like fast wifi or parking, and what you love about the neighborhood</h4>
-      <textarea
-      id="create-text-area"
-      value={description}
-      placeholder="Please write at least 30 characters"
-      onChange={(e) => setDescription(e.target.value)}
-      />
-      {submitted && <span style={{color: "red"}}>{errors.description}</span>}
-    </label>
-    <label className="create-border-bottom">
-      <h2>Create a title for your spot</h2>
-      <h4 className="create-form-subheader">Catch guests&apos; attention with a spot title that highlights what makes your place special</h4>
-      <input
-      id="spot-name-input"
-      type="text"
-      value={name}
-      placeholder="Name of your spot"
-      onChange={(e) => setName(e.target.value)}
-      />
-      {"name" in errors && <span style={{color: "red"}}>{errors.name}</span>}
-    </label>
-    <label className="create-border-bottom">
-      <h2>Set a base price for your spot</h2>
-      <h4>Competitive pricing can help your listing stand out and rank higher in search results.</h4>
-      <span id="money">$ </span>
-      <input
-      id="price-input"
-      type="number"
-      value={price}
-      step={1}
-      min={1}
-      placeholder="Price per night (USD)"
-      onChange={(e) => setPrice(e.target.value)}
-      />
-      {"price" in errors && <span style={{color: "red"}}>{errors.price}</span>}
-    </label>
-    <label className="create-border-bottom" id="photo-inputs">
-      <h2 id="photo-header-form">Liven up your spot with photos</h2>
-      <h4 className="create-form-subheader">Submit a link to at least one photo to publish your spot</h4>
-      <input
-        id="preview-image-url"
-        placeholder="Preview Image URL"
-        value={image1}
-        onChange={(e) => setImage1(e.target.value)}
-      />
-      {"image1" in errors && <span style={{color: "red"}}>{errors.image1}</span>}
-      <input
-      className="image-url-input"
-      placeholder="Image URL"
-      type="text"
-      value={image2}
-        onChange={(e) => setImage2(e.target.value)}
-      />
-      <input
-      className="image-url-input"
-      placeholder="Image URL"
-      type="text"
-      value={image3}
-        onChange={(e) => setImage3(e.target.value)}
-      />
-      <input
-      className="image-url-input"
-      placeholder="Image URL"
-      type="text"
-      value={image4}
-        onChange={(e) => setImage4(e.target.value)}
-      />
-      <input
-      className="image-url-input"
-      placeholder="Image URL"
-      type="text"
-      value={image5}
-        onChange={(e) => setImage5(e.target.value)}
-      />
-    </label>
-    <button id="submit-create-spot" type="submit">Create Spot</button>
-  </form>
-  </div>
-)
+  )
 }
 
 export default CreateNewSpot;
